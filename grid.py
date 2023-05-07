0f47270c76d0586677db3b211acf1b433de29e44
bhjohns01 <bhjohnson556@gmail.com>
2023-05-07 15:35:00 -0700

    A* algorithm
    finished algorithm, robots not properly implemented

--- grid.py
+++ grid.py
@@ -11,88 +11,85 @@ class Node():
         self.h = 0 #estimated distance from end using hypotenuse
         self.f = self.g + self.h #g + h
     
-        def __eq__(self, other):
-            return self.position == other.position
+    def __eq__(self, other):
+        return self.position == other.position
+    def __ne__(self, other):
+        return self.position != other.position
 
 class Robot():
     
-    def __init__(self):
-        orders = []
-        path = []
+    path = []
+    orders = []
+    def __init__(self, path=None, orders=None):
+        orders = orders
+        path = path
 
     def printPath(self):
-        for node in self.path:
-            print("(" + node[0] + ", " + node[1] + ")")
+        print(self.path)
 
 def A_star(grid, start, end, robots):
     #create start and end nodes
     startNode = Node(None, start)
     endNode = Node(None, end)
+    startNode.g = 0
+    startNode.h = (startNode.position[0] - endNode.position[0])**2 + (startNode.position[1] - endNode.position[1])**2 #pythagorean theorem
+    startNode.f = startNode.g + startNode.h
 
-    openNodes = []
     closedNodes = []
-    children = []
+
 
     #run A* for each robot
     for robot in robots:
-        openNodes.append(startNode) 
+        openNode = startNode
 
-        while len(openNodes) > 0:
-            currentNode = openNodes.pop
-            closedNodes.append(currentNode)
-            if currentNode == endNode:
-                #done
-                path = []
-                current = currentNode
-                while current is not None:
-                    path.append(current.position)
-                    current = current.parent
-                    robot.path = path[::-1]
-                    return
-            else:
-                for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
-                    #we check the heuristic of each of these sqaures
-                    #if there's no obstacle and it's unexplored, then we add it to nextNodes array
-                    tempNode = (currentNode.position[0] + newPosition[0], currentNode.position[1] + newPosition[1])
-                    #check bound
-                    if tempNode.position[0] > len(grid[0]) or tempNode.position[1] > len(grid) or tempNode.position[0] < 0 or tempNode.position[1] < 0:
-                        #out of bounds
+        while openNode != endNode:
+             # Get the current node
+            currentNode = openNode
+
+            #move this outside the while loop
+            for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+                #we check the heuristic of each of these sqaures
+                #if there's no obstacle and it's unexplored, then we add it to nextNodes array
+                tempNode = (currentNode.position[0] + newPosition[0], currentNode.position[1] + newPosition[1])
+                #check bound
+                if tempNode[0] >= len(grid[0]) or tempNode[1] >= len(grid) or tempNode[0] < 0 or tempNode[1] < 0:
+                    #out of bounds
+                    continue
+                #check if obstacle
+                if grid[tempNode[0]][tempNode[1]] == 1:
+                    #obstacle
+                    continue
+                #check if already explored
+                childNode = Node(None, tempNode)
+                for node in closedNodes:
+                    if(node == childNode):
                         continue
-                    #check if obstacle
-                    if grid[tempNode.position[0]][tempNode.position[1]] == 1:
-                        #obstacle
-                        continue
-                    #check if already explored
-                    for node in closedNodes:
-                        if(node == closedNodes):
-                            continue
+                #add this node to already explored nodes    
+                closedNodes.append(childNode)
 
-                    childNode = Node(currentNode, tempNode)
-                    childNode.g = currentNode.g + 1
-                    childNode.h = (childNode.position[0] - end.position[0])**2 + (childNode.position[1] - end.position[1])**2 #pythagorean theorem
-                    childNode.f = childNode.g + childNode.h
-                    children.append(childNode)
+                childNode.g = currentNode.g + 1
+                childNode.h = (childNode.position[0] - endNode.position[0])**2 + (childNode.position[1] - endNode.position[1])**2 #pythagorean theorem
+                childNode.f = childNode.g + childNode.h
+                
+                if childNode.f <= openNode.f:
+                    #finding the best heuristic out of the adjacent nodes
+                    childNode.parent = currentNode
+                    openNode = childNode
+        path = []
+        current = openNode
+        while current is not None:
+            path.append(current.position)
+            current = current.parent
+            robot.path = path[::-1]
+        return
 
-                    #check to see if node already is in open list
-                    for node in openNodes:
-                        if childNode == node and childNode.g > node.g:
-                            continue
-
-                    if childNode.f < currentNode.f:
-                        #favorable heuristic
-                        openNodes.append(childNode)
-                    else:
-                        #unfavorable heuristic
-                        closedNodes.append(childNode)
 def main():
 
     NUMBER_OF_ROBOTS = 10
-    GOAL = (7,6)
+    GOAL = (9,9)
     START = (0, 0)
 
-    robots = []
-    for robot in robots:
-          robots.append(robot)
+    robots = [Robot() for _ in range(NUMBER_OF_ROBOTS)]
 
     #create board
     rows, cols = (10,10)
